2616b97de092746a0cadd53b160e171a9396b609
JUHO <ijuho9555@gmail.com>
2025-08-07 16:22:33 +0900

    얼굴 검출 후 좌표를 원본 이미지 기준으로 반환시킴
    좌,우로 반전시켰을 때 화면에서 모자이크도 반전되어 출력되는 경우가 있어 원본 이미지 기준으로 좌표 반환시켜 오류 수정

--- src/mosaic_face.py
+++ src/mosaic_face.py
@@ -8,33 +8,43 @@ profile_face_cascade = cv2.CascadeClassifier('./data/haarcascade_frontalface_alt
 # 카메라 캡쳐 활성화
 cap = cv2.VideoCapture(0)
 while cap.isOpened():    
-    ret, img = cap.read()  # 프레임 읽기
+    ret, frame = cap.read()  # 프레임 읽기
     rate = 15
     if ret:
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame_w = frame.shape[1]
+        
+        # faces_all을 리스트 형식으로 만들기
+        faces_all = []
+
         # 정면 얼굴 검출    
         faces = frontal_face_cascade.detectMultiScale(gray, scaleFactor=1.3, \
                                         minNeighbors=5, minSize=(80,80))
+        for (x, y, w, h) in faces:
+            faces_all.append((x, y, w, h)) # 좌표 변환
+
         # 왼쪽 측면 얼굴 검출
         profiles = profile_face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(80, 80))
+        for (x, y, w, h) in profiles:
+            faces_all.append((x, y, w, h)) # 좌표를 원본 이미지 기준으로 변환
 
         # 오른쪽 측면 얼굴 검출
         gray_flipped = cv2.flip(gray, 1)
-        faces_profile_flipped = profile_face_cascade.detectMultiScale(gray_flipped)
+        profiles_flipped = profile_face_cascade.detectMultiScale(gray_flipped, 1.3, 5)
+        for (x, y, w, h) in profiles_flipped:
+            x_original = frame_w - (x + w)
+            faces_all.append((x_original, y, w, h)) # 좌표 변환
 
-        # 모든 얼굴 리스트 통합
-        all_faces = list(faces) + list(profiles) + list(faces_profile_flipped)
-
-        for(x,y,w,h) in all_faces:
-            cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255,0),2)
+        for(x,y,w,h) in faces_all:
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255,0),2)
             # 얼굴부분 가져오기
-            face = img[y:y+h, x:x+w]
+            face = frame[y:y+h, x:x+w]
 
             small = cv2.resize(face, (w//rate, h//rate)) # 1/rate 비율로 축소
             # 원래 크기로 확대
             mosaic = cv2.resize(small, (w,h), interpolation=cv2.INTER_AREA)  
-            img[y:y+h, x:x+w] = mosaic
-        cv2.imshow('face mosaic', img)
+            frame[y:y+h, x:x+w] = mosaic
+        cv2.imshow('face mosaic', frame)
     else:
         break
     if cv2.waitKey(5) == 27:
